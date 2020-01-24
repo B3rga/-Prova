8dd439b374dcfc27ba154ff053964d49df5be334
barbioalessandro <57799321+barbioalessandro@users.noreply.github.com>
2020-01-24 13:45:25 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Hello {
 
    public static void main(String[] args) {
-gh
-      System.out.println("ambarabaciccico!");
-ccogvhft
+
+      System.out.println("ciao mondo");
+
    }
 }
